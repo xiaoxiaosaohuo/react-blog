7df548d66caf600a9dfa1d091a2543afffc8cb9f
金 鑫 <jinxin@hualala.com>
2017-11-14 09:44:02 +0800

    完成打赏

--- src/components/reward/index.js
+++ src/components/reward/index.js
@@ -1,7 +1,12 @@
 import React, { PureComponent } from 'react';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
-import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
 import orange from 'material-ui/colors/orange';
 
 const primary = orange[700];
@@ -18,23 +23,54 @@ const styles = theme => ({
           background: primary,
           color:"#fff"
       }
+  },
+  rewardWrapper:{
+      display:'flex'
   }
 
   })
 
   class Reward extends PureComponent {
+      constructor(props){
+          super(props)
+          this.state={
+              visible:false
+          }
+
+      }
+      onClick = ()=>{
+          this.setState({
+              visible:true
+          })
+      }
+      onClose = ()=>{
+          this.setState({
+              visible:false
+          })
+      }
       render(){
           const {classes} = this.props;
+          const {visible} = this.state;
           return(
-              <div    >
+              <div>
                   <Button
                       fab
                       aria-label="money"
                       className={classes.money}
                       title="赏"
+                      onClick = {this.onClick}
                       >
                       <span>赏</span>
                   </Button>
+                  <Dialog open={visible} onRequestClose = {this.onClose}>
+                      <DialogTitle>谢谢您的鼓励！</DialogTitle>
+                      <DialogContent>
+                          <div className={classes.rewardWrapper}>
+                               <img src="/reward.jpg" width='300' height="100%"></img>
+                          </div>
+
+                      </DialogContent>
+                  </Dialog>
 
               </div>
           )
